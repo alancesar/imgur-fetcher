d0827804a995b186e7eb66a26905ac7e13403df2
Alan Cesar Elias <alancesarelias@gmail.com>
2023-11-12 18:00:38 -0300

    refactor: remove dead code

--- pkg/media/media.go
+++ pkg/media/media.go
@@ -1,15 +1,8 @@
 package media
 
-import "path"
-
 type (
 	Media struct {
-		URL      string   `json:"url"`
-		Filename string   `json:"filename"`
-		Parent   []string `json:"parent"`
+		URL    string   `json:"url"`
+		Parent []string `json:"parent"`
 	}
 )
-
-func (m Media) Path() string {
-	return path.Join(path.Join(m.Parent...), m.Filename)
-}
